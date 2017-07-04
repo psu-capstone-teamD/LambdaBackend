55d2e2afe144e4c2000eb1c53f3b1cbc83735782
Randy Perrone <mrrandyperrone@gmail.com>
2017-07-04 11:27:53 -0700

    Delete Consts.py

--- Consts.py
+++
@@ -1,6 +0,0 @@
-class Consts:
-    def __init__(self):
-        self.CONST_LIVEXMLSTORAGE = 'livexmlstorage'
-        self.CONST_BXFSTORAGE = 'bxfstorage'
-        self.CONST_ACCESSKEY = 'AKIAJLFWURK7YOJ7MLEA'
-        self.CONST_SECRETACCESSKEY = '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'
