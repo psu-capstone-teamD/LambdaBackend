362ceaa97c505e0719f641d588a9337f417a5a38
randyperrone <mrrandyperrone@gmail.com>
2017-07-14 14:31:03 -0700

    dealing with git problems

--- S3Service.py
+++ S3Service.py
@@ -1,6 +1,4 @@
 import boto3
-from botocore.client import Config
-import os
 
 class S3Service:
     def __init__(self):
@@ -11,16 +9,23 @@ class S3Service:
 
     def storexml(self, bucketName, filename, xml_file):
         if(bucketName is not self.livexmlstorage and bucketName is not self.bxfstorage):
-            return
+            return "Bucket name is incorrect"
+
         if(not isinstance(filename, basestring)):
-            return
-        if(not  isinstance(xml_file, file)):
-            return
+            return "File name must be a string"
+
+        if (not isinstance(xml_file, basestring)):
+            return "Not a valid .xml string"
+
         conn = boto3.resource('s3',
                               aws_access_key_id=self.access_key,
-                              aws_secret_access_key=self.secret_key,
-                              config=Config(signature_version='s3v4'))
-        conn.Bucket(bucketName).put_object(Key=filename, Body=xml_file)
+                              aws_secret_access_key=self.secret_key)
+
+        try:
+            conn.Bucket(bucketName).put_object(Key=filename, Body=xml_file)
+            return  "statusCode': '200"
+        except Exception as e:
+            return "statusCode': '400"
 
 
     def getxml(self, bucketName, filename):
@@ -30,11 +35,9 @@ class S3Service:
             return
         # Get the service client
         s3 = boto3.client('s3', aws_access_key_id=self.access_key,
-                              aws_secret_access_key=self.secret_key,
-                              config=Config(signature_version='s3v4'))
+                          aws_secret_access_key=self.secret_key)
 
-        # Download object at bucket-name with key-name to filename
-        nameOfStoredFile = "tempfile.xml"
-        s3.download_file(bucketName, filename, nameOfStoredFile)
-        #return the downloaded object, r+b is reading/writing and binary
-        return open(nameOfStoredFile, 'r+b')
+        try:
+            return s3.get_object(Bucket = bucketName, Key = filename)
+        except Exception as e:
+            return "statusCode': '400"
--- TestS3Service.py
+++ TestS3Service.py
@@ -2,34 +2,45 @@ import unittest
 from S3Service import S3Service
 
 class MyTestCase(unittest.TestCase):
-    def test_storexml_live_bucket(self):
-        s3 = S3Service()
-        s3.storexml('livexmlstorage', 'afilename.xml', 'afile.xml')
 
-    def test_storexml_bxf_bucket(self):
+    def test_storexml__live(self):
         s3 = S3Service()
-        s3.storexml('bxfstorage', 'afilename.xml', 'afile.xml')
+        data = "this is some text to put into s3"
+        print(s3.storexml('livexmlstorage', 'afilename.xml', data))
+
+    def test_storexml_bxf(self):
+        s3 = S3Service()
+        data = "this is some text to put into s3"
+        print(s3.storexml('bxfstorage', 'afilename.xml', data))
+
+    def test_storexml_bxf_isAFile(self):
+        s3 = S3Service()
+        data = "this is some text to put into s3"
+        print(s3.storexml('bxfstorage', 'afilename.xml', data))
+
+    def test_storexml_live_inputnotaString(self):
+        s3 = S3Service()
+        print(s3.storexml('livexmlstorage', 'afilename.xml', 1))
+
+    def test_storexml_bxf_inputnotaString(self):
+        s3 = S3Service()
+        print(s3.storexml('bxfstorage', 'afilename.xml', 1))
 
     def test_storexml_wrong_bucket(self):
         s3 = S3Service()
-        s3.storexml('incorrectBucketName', 'afilename.xml', 'afile.xml')
+        print(s3.storexml('incorrectBucketName', 'afilename.xml', 'afile.xml'))
 
     def test_storexml_wrong_bucket(self):
         s3 = S3Service()
-        s3.storexml(1, 'afilename.xml', 'afile.xml')
+        print(s3.storexml(1, 'afilename.xml', 'afile.xml'))
 
     def test_storexml_wrong_filename(self):
         s3 = S3Service()
-        s3.storexml('livexmlstorage', 1, 'afile.xml')
+        print(s3.storexml('livexmlstorage', 1, 'afile.xml'))
 
     def test_storexml_notAFile(self):
         s3 = S3Service()
-        s3.storexml('livexmlstorage', 'afilename.xml', 'afile.xml')
-
-    def test_storexml_isAFile(self):
-        s3 = S3Service()
-        data = open('sample.xml', 'rb')
-        s3.storexml('livexmlstorage', 'afilename.xml', data)
+        print(s3.storexml('livexmlstorage', 'afilename.xml', 'afile.xml'))
 
     def test_getxml_live_bucket(self):
         s3 = S3Service()
@@ -43,9 +54,17 @@ class MyTestCase(unittest.TestCase):
         s3 = S3Service()
         s3.getxml('incorrectBucketName', 'afilename.xml')
 
-    def test_storexml_wrong_filename(self):
+    def test_getxml_wrong_filename(self):
         s3 = S3Service()
         s3.getxml('livexmlstorage', 1)
+
+    def test_getxml_wrong_Live_filename(self):
+        s3 = S3Service()
+        s3.getxml('livexmlstorage', "somenonexistentfile.xml")
+
+    def test_getxml_wrong_bxf_filename(self):
+        s3 = S3Service()
+        s3.getxml('bxfstorage', "somenonexistentfile.xml")
 
 
 if __name__ == '__main__':
