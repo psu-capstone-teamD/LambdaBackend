9c4587dc098c65f0408b9d381427fc5a9e381aae
Randy Perrone <mrrandyperrone@gmail.com>
2017-08-17 10:04:39 -0700

    Changed TestSchedulerController to fix travis errors

--- TestSchedulerController.py
+++ TestSchedulerController.py
@@ -12,19 +12,6 @@ class MyTestCase(unittest.TestCase):
         schedControl = SchedulerController()
         print(schedControl.storebxffile('afilename.xml', 1))
 
-    def test_storelivefile_wrong_filename(self):
-        schedControl = SchedulerController()
-        data = "This is some data to put into s3"
-        print(schedControl.storelivefile(1, data))
-
-    def test_storelivefile_wrong_filetype(self):
-        schedControl = SchedulerController()
-        print(schedControl.storelivefile('afilename.xml', 1))
-
-    def test_loadlivefile_wrong_filename(self):
-        schedControl = SchedulerController()
-        print(schedControl.loadLiveFile(1))
-
     def test_sendtolive_wrong_filename(self):
         schedControl = SchedulerController()
         print(schedControl.createLiveEvent(1))
